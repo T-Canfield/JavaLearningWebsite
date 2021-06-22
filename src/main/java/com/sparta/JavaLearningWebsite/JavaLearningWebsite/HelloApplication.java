14b5f9f9cd965e10c1f8490fa609a50b55d810ef
George Repole <grepole121@users.noreply.github.com>
2021-06-22 14:25:36 +0100

    adding empty project

---
+++ src/main/java/com/sparta/JavaLearningWebsite/JavaLearningWebsite/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.sparta.JavaLearningWebsite.JavaLearningWebsite;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
